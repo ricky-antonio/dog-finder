e95e94e2583965bf88d8a8ebb3ad0ccb70484b29
Ricardo Monterrosa <rickyantonio117@gmail.com>
2021-02-16 18:57:47 -0800

    Initial Commit

---
+++ src/DogList.js
@@ -0,0 +1,24 @@
+import React, {Component} from 'react';
+import './DogList.css';
+
+class DogList extends Component {
+    render() {
+        return (
+            <div className="DogList">
+                <h1 className="display-1 text-center">dogs list component</h1>
+                <div className="container">
+                    <div className="row">
+                        {this.props.dogs.map(d => (
+                            <div className="DogList-dog col-md-6 col-lg-4 text-center" key={d.name}>
+                                <h2>{d.name}</h2>
+                                <img src={d.src} alt={d.name}/>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default DogList;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
+import App from './App';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter} from 'react-router-dom';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
